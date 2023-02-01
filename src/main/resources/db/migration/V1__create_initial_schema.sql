7c8a054ddba6164823093aa4ee91cfceff9202ea
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2023-02-01 13:14:48 -0300

    Implementando versionamento do banco com flyway

---
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE _user(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(255)
+);
+
+INSERT INTO _user(email, password, role)
+VALUES ('admin@email.com', '$2a$10$6W9lFh22y.yDfVIh8ZASa.iyqZNbJpuP1VM3ofQWHUFcyxG0d6TJq', 'ADMIN');
+
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    price NUMERIC(38,2)
+);
+
+CREATE TABLE sale (
+    id SERIAL PRIMARY KEY,
+    date DATE,
+    status VARCHAR(255),
+    _user bigint
+);
+
+CREATE TABLE sale_products (
+    sale_id bigint NOT NULL,
+    products_id bigint NOT NULL
+);
