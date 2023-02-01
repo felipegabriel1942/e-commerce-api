807136226ccfd05a4c74b54a76f3edd8ce6ea025
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2023-02-01 13:22:05 -0300

    Removendo classe UserDTO

--- src/test/java/com/felipegabriel/ecommerceapi/commom/UserConstants.java
+++ src/test/java/com/felipegabriel/ecommerceapi/commom/UserConstants.java
@@ -1,12 +1,10 @@
 package com.felipegabriel.ecommerceapi.commom;
 
-import com.felipegabriel.ecommerceapi.dto.UserDTO;
 import com.felipegabriel.ecommerceapi.enums.Role;
 import com.felipegabriel.ecommerceapi.model.entity.User;
 
 public class UserConstants {
 
     public static final User USER = User.builder().email("user@mail.com").role(Role.USER).build();
-    public static final UserDTO USER_DTO = UserDTO.builder().email("user@mail.com").role(Role.USER).build();
     public static final User USER_ADMIN = User.builder().email("admin@mail.com").role(Role.ADMIN).build();
 }
