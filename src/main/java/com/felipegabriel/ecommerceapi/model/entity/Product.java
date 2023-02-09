0fe9b53754932719905acb0d2f26184b583d8a7b
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2023-02-09 11:02:02 -0300

    Criando testes de integração para o ProductRepository

--- src/main/java/com/felipegabriel/ecommerceapi/model/entity/Product.java
+++ src/main/java/com/felipegabriel/ecommerceapi/model/entity/Product.java
@@ -1,6 +1,10 @@
 package com.felipegabriel.ecommerceapi.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,7 +24,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @Column(nullable = false)
     private String name;
 
+    @NotNull
+    @Column(nullable = false)
+    @DecimalMin(value = "0.1", inclusive = false)
     private BigDecimal price;
 }
---
+++ src/test/java/com/felipegabriel/ecommerceapi/repository/ProductRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.felipegabriel.ecommerceapi.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.felipegabriel.ecommerceapi.commom.ProductConstants;
+import com.felipegabriel.ecommerceapi.model.entity.Product;
+import com.felipegabriel.ecommerceapi.model.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createProduct_WithValidData_ReturnsProduct() {
+        Product product = productRepository.save(ProductConstants.PRODUCT);
+
+        Product sut = testEntityManager.find(Product.class, product.getId());
+
+        assertThat(sut).isNotNull();
+    }
+
+    @Test
+    public void createProduct_WithInvalidData_ThrowsException() {
+        Product emptyProduct = new Product();
+        Product invalidProduct = new Product(null, "", BigDecimal.ZERO);
+
+        assertThatThrownBy(() ->  productRepository.save(emptyProduct)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() ->  productRepository.save(invalidProduct)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void findProduct_ByName_ReturnsProducts() {
+        productRepository.save(ProductConstants.PRODUCT);
+
+        List<Product> sut = productRepository.findByName(ProductConstants.PRODUCT.getName());
+
+        assertThat(sut.isEmpty()).isFalse();
+        assertThat(sut.size()).isEqualTo(1);
+        assertThat(sut.get(0)).isEqualTo(ProductConstants.PRODUCT);
+    }
+}
